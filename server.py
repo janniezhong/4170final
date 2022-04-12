3cc6d9457a8cfd7a4b9f0d1c2df57eb9410a942b
Jannie Zhong <24399232+janniezhong@users.noreply.github.com>
2022-04-12 11:30:18 -0400

    initial template

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+app = Flask(__name__)
+
+# ROUTES
+
+@app.route('/')
+def home():
+    return render_template('index.html') 
+
+# ajax
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
+
