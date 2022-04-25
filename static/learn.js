4eb7d17f59b3db11e1e248ed4fa940bee3082acc
Jannie Zhong <24399232+janniezhong@users.noreply.github.com>
2022-04-25 14:24:39 -0400

    remove extra code

--- static/learn.js
+++ static/learn.js
@@ -117,7 +117,6 @@ $(document).ready(function(){
     updateCurrLesson(lesson_info)
     displayCurrLesson()
 
-    // getNextLesson()
 
     term.onKey(e => {
         let code  = e.key.charCodeAt()
