c648390bf39107a07023e745129c2c1cb9efa6d2
afsong <52013054+afsong@users.noreply.github.com>
2022-05-02 10:25:39 -0400

    Afs2185/features update (#35)
    * add support for copy-paste
    
    * add cursor movement

--- static/learn.js
+++ static/learn.js
@@ -16,9 +16,12 @@ let currLesson = {
 
 let lessonFinished = false
 
+let keyboardContent = ""
+let cursorPos = 0
+
 var term;
 
-function updateCurrLesson(lesson_info){
+function updateCurrLesson(lesson_info) {
     currLesson["chapter"] = lesson_info["chapter"]
     currLesson["chapter_id"] = lesson_info["chapter_id"]
     currLesson["lesson_id"] = lesson_info["lesson_id"]
@@ -32,7 +35,7 @@ function updateCurrLesson(lesson_info){
     currLesson["next_lesson_id"] = lesson_info["next_lesson_id"]
 }
 
-function displayCurrLesson(){
+function displayCurrLesson() {
     // display title
     $(".title").empty()
     console.log(currLesson["topic"])
@@ -46,15 +49,13 @@ function displayCurrLesson(){
     $(".instruction").empty()
     $(".instruction").text(currLesson["instruction"])
 
-    $("#prev").attr("href", "/learn/"+currLesson["previous_lesson_id"])
-    if (currLesson["lesson_id"] == "final"){
-        $("#next").attr("href", "/quiz/1")
-    } else {
-        $("#next").attr("href", "/learn/"+currLesson["next_lesson_id"])
-    }
+
+    $("#prev").attr("href", "/learn/" + currLesson["previous_lesson_id"])
+
 }
 
-function checkAnswer(){
+function checkAnswer() { // if the lesson is the last one, go to the quiz instead
+
 
     data = {
         "id": currLesson["lesson_id"],
@@ -62,23 +63,23 @@ function checkAnswer(){
     }
     $.ajax({
         type: "POST",
-        url: "/check_answer",                
-        dataType : "json",
+        url: "/check_answer",
+        dataType: "json",
         contentType: "application/json; charset=utf-8",
-        data : JSON.stringify(data),
-        success: function(result){
+        data: JSON.stringify(data),
+        success: function (result) {
             console.log("success!")
             console.log(result)
             let correct = result["correct"]
             let error = result["error"]
-            if (correct == "true"){
+            if (correct == "true") {
                 updateTerminal(currLesson["response"])
                 finishLesson()
             } else {
                 updateTerminal(error)
             }
         },
-        error: function(request, status, error){
+        error: function (request, status, error) {
             console.log("Error");
             console.log(request)
             console.log(status)
@@ -86,16 +87,29 @@ function checkAnswer(){
         }
     });
 }
-function finishLesson(){
+function finishLesson() {
 
-    $("#feedback").empty()
-    $("#feedback").append(currLesson["feedback"])
+    // pop up modal
+    $("#modal-button").trigger("click")
+    $(".modal-body").empty()
+    $(".modal-body").text(currLesson["feedback"])
+
+    // allow next button
+    if (currLesson["lesson_id"] == "final") {
+        $("#next").attr("href", "/quiz/1")
+    } else {
+        $("#next").attr("href", "/learn/" + currLesson["next_lesson_id"])
+    }
+
+//     $("#feedback").empty()
+//     $("#feedback").append(currLesson["feedback"])
+
 
     // disallow typing in terminal
     // term.onKey(e => {})
 }
 
-function updateTerminal(s){
+function updateTerminal(s) {
     term.write('\r\n')
     term.write(s)
     term.write('\r\n> ')
@@ -103,61 +117,122 @@ function updateTerminal(s){
 
 function setProgBar(id) {
     var elem = document.getElementById("lesson-prog-bar");
-    var val = Math.round(100/14 * (parseInt(id)-1));
+    var val = Math.round(100 / 14 * (parseInt(id) - 1));
     console.log(val)
     elem.setAttribute("style", "width: " + val.toString() + "%;");
     elem.setAttribute("aria-valuenow", val.toString());
-  
-    $("#lesson-prog-bar").text(val + "%");
-  }
 
-  let els = document.getElementsByClassName('step');
-  let steps = [];
-  
-  function progress(stepNum) {
+    $("#lesson-prog-bar").text(val + "%");
+}
+
+let els = document.getElementsByClassName('step');
+let steps = [];
+
+function progress(stepNum) {
     let p = stepNum * 15;
     document.getElementsByClassName('percent_1')[0].style.width = `${p}%`;
     document.getElementsByClassName('percent_2')[0].style.width = `${p}%`;
     steps.forEach((e) => {
-      if (e.id <= stepNum) {
-        e.classList.add('completed');
-      }
-      if (e.id > stepNum) {
-        e.classList.remove('selected', 'completed');
-      }
+        if (e.id <= stepNum) {
+            e.classList.add('completed');
+        }
+        if (e.id > stepNum) {
+            e.classList.remove('selected', 'completed');
+        }
     });
-  }
+}
 
 
-$(document).ready(function(){
-    term = new Terminal({cursorBlink: "block", cols: 75, rows: 13, fontSize: 13,theme: {
-        background: '#434343ff',
-      }});
+function getContentFromClipboard() {
+    navigator.clipboard
+        .readText()
+        .then((copiedText) => {
+            keyboardContent = copiedText;
+        });
+}
+
+function putContentToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        console.log("into clipboard now")
+    }, () => {
+        console.log("rejected bro")
+    });
+}
+
+$(document).ready(function () {
+    term = new Terminal({
+        cursorBlink: "block", cols: 50, rows: 22, theme: {
+            background: '#8d8b8bff'
+        }
+    });
+
     term.open(document.getElementById('terminal'));
     term.write("> ")
     console.log(lesson_info)
 
     updateCurrLesson(lesson_info)
     displayCurrLesson()
-    if (currLesson["lesson_id"] == "final"){
+    if (currLesson["lesson_id"] == "final") {
         setProgBar("14")
     } else {
         setProgBar(currLesson["lesson_id"])
     }
 
-    term.onKey(e => {
-        let code  = e.key.charCodeAt()
+    term.attachCustomKeyEventHandler((arg) => {
+        getContentFromClipboard();
 
-        if (code == 13){
-            if (lessonFinished){
+        if (arg.ctrlKey && arg.code === "KeyC" && arg.type === "keydown") {
+            const selection = term.getSelection();
+            if (selection) {
+                putContentToClipboard(selection);
+                return false;
+            }
+        }
+
+        if (arg.ctrlKey && arg.code === "KeyV" && arg.type === "keydown") {
+            term.write(keyboardContent);
+            currLine = keyboardContent;
+            cursorPos = currLine.length;
+            return false;
+        }
+
+        return true;
+    });
+
+    term.onKey(e => {
+        let code = e.key.charCodeAt()
+
+        if (code == 13) {
+            if (lessonFinished) {
                 window.location.href = "/quiz/1"
             } else if (currLine) {
                 checkAnswer()
-                currLine=""
+                currLine = ""
             }
         } else if (code < 32) { // Control
-            return;
-        } else if (code == 127 || code == 8){
+            if (code != 27) {
+                return;
+            }
+
+            switch (e.key) {
+                case '\x1b[D':
+                    if (cursorPos > 0) {
+                        cursorPos--;
+                        term.write(e.key);
+                    }
+                    break;
+
+                case '\x1b[C':
+                    if (cursorPos < currLine.length) {
+                        cursorPos++;
+                        term.write(e.key)
+                    }
+                    break
+
+                default:
+                    break;
+            }
+        } else if (code == 127 || code == 8) {
             if (currLine) {
                 currLine = currLine.slice(0, currLine.length - 1);
                 term.write("\b \b");
@@ -165,6 +240,7 @@ $(document).ready(function(){
         } else {
             currLine += e.key;
             term.write(e.key);
+            cursorPos++;
         }
 
     })
@@ -177,4 +253,4 @@ $(document).ready(function(){
         }
     });
 
-}) 
+})
--- static/quiz.js
+++ static/quiz.js
@@ -1,190 +1,259 @@
 var term;
 
+var container = {
+    currLine: "",
+    keyboardContent: "",
+    cursorPos: 0
+};
+
 function checkAnswer(currLine, qid) {
-  data = {
-    id: qid,
-    response: currLine,
-  };
+    data = {
+        id: qid,
+        response: currLine,
+    };
 
-  console.log(currLine);
+    console.log(currLine);
 
-  $.ajax({
-    type: "POST",
-    url: "/save_response",
-    dataType: "json",
-    contentType: "application/json; charset=utf-8",
-    data: JSON.stringify(data),
-    success: function (result) {
-      let res = result[qid];
+    $.ajax({
+        type: "POST",
+        url: "/save_response",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+        success: function (result) {
+            let res = result[qid];
 
-      if (res == 1) {
-        // if the answer is correct
-        $("#next").attr("href", getNextPage(qid));
-      } else {
-        term.write("\r\n That wasn't quite right :/ Try again? \r\n > ");
-      }
-    },
-    error: function (request, status, error) {
-      console.log("Error");
-      console.log(request);
-      console.log(status);
-      console.log(error);
-    },
-  });
+            if (res == 1) {
+                $("#next").attr("href", getNextPage(qid));
+                term.write("\r\n Nice! Click next to go to the next page!\r\n");
+            } else {
+                term.write("\r\n That wasn't quite right :/ Try again? \r\n > ");
+            }
+        },
+        error: function (request, status, error) {
+            console.log("Error");
+            console.log(request);
+            console.log(status);
+            console.log(error);
+        },
+    });
 }
 
 function getPrevPage(qid) {
-  if (qid == 1) {
-    return "/learn/final";
-  } else {
-    return "/quiz/" + (qid - 1).toString();
-  }
+    if (qid == 1) {
+        return "/learn/final";
+    } else {
+        return "/quiz/" + (qid - 1).toString();
+    }
 }
 
 function getNextPage(qid) {
+
+    // if the lesson is the last one, go to the quiz instead
+    if (qid == 4) {
+        return "/quiz/result";
+    } else {
+        return "/quiz/" + (qid + 1).toString();
+    }
+
   // if the lesson is the last one, go to the quiz instead
-  if (qid == 4) {
-    return "/result";
-  } else {
-    return "/quiz/" + (qid + 1).toString();
-  }
+
+
 }
 
 function updateTerminal(s) {
-  term.write(s);
-  term.write("\r\n > ");
+    term.write(s);
+    term.write("\r\n > ");
 }
 
 function createHeader() {
-  let hline = "-".repeat(80);
-  let vline = "|" + " ".repeat(78) + "|";
-  let title =
-    "|" + " ".repeat(26) + "Searching Through Recipes" + " ".repeat(27) + "|";
+    let hline = "-".repeat(80);
+    let vline = "|" + " ".repeat(78) + "|";
+    let title =
+        "|" + " ".repeat(26) + "Searching Through Recipes" + " ".repeat(27) + "|";
 
-  const arr = [hline, vline, vline, title, vline, vline, hline];
+    const arr = [hline, vline, vline, title, vline, vline, hline];
 
-  return arr.join("\r\n");
+    return arr.join("\r\n");
 }
 
 function getCurrQuizNum() {
-  return window.location.href.trim().slice(-1);
+    return window.location.href.trim().slice(-1);
 }
 
 function renderTreeOne() {
-  term.write("recipe_book \r\n");
-  term.write("├── recipe1 \r\n");
-  term.write("├── recipe2 \r\n");
-  term.write("├── recipe3 \r\n");
-  term.write("└── recipe4 \r\n");
+    term.write("recipe_book \r\n");
+    term.write("├── recipe1 \r\n");
+    term.write("├── recipe2 \r\n");
+    term.write("├── recipe3 \r\n");
+    term.write("└── recipe4 \r\n");
 
-  term.write("0 directories, 4 files");
+    term.write("0 directories, 4 files");
 
-  // $( ".instruction" ).empty();
+    // $( ".instruction" ).empty();
 
-  // $('<p>' + "recipe_book \r\n"+"├── recipe1 \r\n"+"├── recipe2 \r\n"+ "├── recipe3 \r\n"+"└── recipe4 \r\n"+ '</p>').appendTo('.instruction');
+    // $('<p>' + "recipe_book \r\n"+"├── recipe1 \r\n"+"├── recipe2 \r\n"+ "├── recipe3 \r\n"+"└── recipe4 \r\n"+ '</p>').appendTo('.instruction');
 }
 
 function renderTreeTwo() {
-  term.write("recipe_book \r\n");
-  term.write("├── savory \r\n");
-  term.write("│   ├── recipe1\r\n");
-  term.write("│   └── recipe2\r\n");
-  term.write("└── sweet\r\n");
-  term.write("    ├── recipe3\r\n");
-  term.write("    └── recipe4\r\n");
+    term.write("recipe_book \r\n");
+    term.write("├── savory \r\n");
+    term.write("│   ├── recipe1\r\n");
+    term.write("│   └── recipe2\r\n");
+    term.write("└── sweet\r\n");
+    term.write("    ├── recipe3\r\n");
+    term.write("    └── recipe4\r\n");
 
-  term.write("2 directories, 4 files");
+    term.write("2 directories, 4 files");
 
-  // $( ".instruction" ).empty();
+    // $( ".instruction" ).empty();
 
-  // $('<p>' + "recipe_book \r\n"+"├── savory \r\n"+"│   ├── recipe1\r\n"+ "│   └── recipe2\r\n"+"└── sweet\r\n"+"    ├── recipe3\r\n"+"    └── recipe4\r\n"+ '</p>').appendTo('.instruction');
+    // $('<p>' + "recipe_book \r\n"+"├── savory \r\n"+"│   ├── recipe1\r\n"+ "│   └── recipe2\r\n"+"└── sweet\r\n"+"    ├── recipe3\r\n"+"    └── recipe4\r\n"+ '</p>').appendTo('.instruction');
 }
 
 function renderTree(qid) {
-  switch (qid) {
-    case 1:
-    case 2:
-    case 3:
-      renderTreeOne();
-      return;
-    case 4:
-      renderTreeTwo();
-    default:
-      return;
-  }
+    switch (qid) {
+        case 1:
+        case 2:
+        case 3:
+            renderTreeOne();
+            return;
+        case 4:
+            renderTreeTwo();
+        default:
+            return;
+    }
 }
 function addText() {
-  $(".prompt").empty();
-  $("<p>" + quiz_info["instruction"] + "</p>").appendTo(".prompt");
-  $("<p>" + "\r\n\r\n" + "</p>").appendTo(".prompt");
+    $(".prompt").empty();
+    $("<p>" + quiz_info["instruction"] + "</p>").appendTo(".prompt");
+    $("<p>" + "\r\n\r\n" + "</p>").appendTo(".prompt");
 
-  $("<p>" + quiz_info["question"] + "</p>").appendTo(".prompt");
+    $("<p>" + quiz_info["question"] + "</p>").appendTo(".prompt");
 }
 
 function addTitle(pid) {
-  $(".title").empty();
-  $("<p>" + "Quiz " + pid + "</p>").appendTo(".title");
+    $(".title").empty();
+    $("<p>" + "Quiz " + pid + "</p>").appendTo(".title");
 }
 
 function setProgBar(qid) {
-  var elem = document.getElementById("quiz-prog-bar");
-  var val = 25 * (qid - 1);
-  elem.setAttribute("style", "width: " + val.toString() + "%;");
-  elem.setAttribute("aria-valuenow", val.toString());
+    var elem = document.getElementById("quiz-prog-bar");
+    var val = 25 * (qid - 1);
+    elem.setAttribute("style", "width: " + val.toString() + "%;");
+    elem.setAttribute("aria-valuenow", val.toString());
 
-  $("#quiz-prog-bar").text(val + "%");
+    $("#quiz-prog-bar").text(val + "%");
+}
+
+
+function getContentFromClipboard() {
+    navigator.clipboard
+        .readText()
+        .then((copiedText) => {
+            container.keyboardContent = copiedText;
+        });
+}
+
+
+function putContentToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        console.log("into clipboard now")
+    }, () => {
+        console.log("rejected bro")
+    });
 }
 
 $(document).ready(function () {
-  addText();
+    addText();
 
-  term = new Terminal({cursorBlink: "block", cols: 75, rows: 13, fontSize: 13,theme: {
-    background: '#434343ff',
-  }});
+    term = new Terminal({
+        cursorBlink: "block",
+        cols: 50,
+        rows: 22,
+        theme: {
+            background: "#8d8b8bff",
+        },
+    });
 
-  term.open(document.getElementById("terminal"));
-  term.write("> ")
+    term.open(document.getElementById("terminal"));
 
-  var qidNum = parseInt(getCurrQuizNum());
+    var qidNum = parseInt(getCurrQuizNum());
 
-  addTitle(qidNum);
-  renderTree(qidNum);
-  setProgBar(qidNum);
+    addTitle(qidNum);
+    renderTree(qidNum);
+    setProgBar(qidNum);
 
-  updateTerminal("");
+    updateTerminal("");
 
-  var container = {
-    currLine: "",
-  };
+    $("#prev").attr("href", getPrevPage(qidNum));
 
-  container.currLine = "";
+    term.attachCustomKeyEventHandler((arg) => {
+        getContentFromClipboard();
 
-  $("#prev").attr("href", getPrevPage(qidNum));
-  $("#next").attr("href", getNextPage(qidNum));
+        if (arg.ctrlKey && arg.code === "KeyC" && arg.type === "keydown") {
+            const selection = term.getSelection();
+            if (selection) {
+                putContentToClipboard(selection);
+                return false;
+            }
+        }
 
+        if (arg.ctrlKey && arg.code === "KeyV" && arg.type === "keydown") {
+            term.write(container.keyboardContent);
+            container.currLine = container.keyboardContent;
+            container.cursorPos = container.currLine.length;
+            return false;
+        }
 
-  term.onKey((e) => {
-    let code = e.key.charCodeAt();
+        return true;
+    });
 
-    if (code == 13) {
-      if (container.currLine) {
-        checkAnswer(container.currLine, qidNum);
-        container.currLine = "";
-      }
-    } else if (code < 32) {
-      // Control
-      return;
-    } else if (code == 127 || code == 8) {
-      if (container.currLine) {
-        container.currLine = container.currLine.slice(
-          0,
-          container.currLine.length - 1
-        );
-        term.write("\b \b");
-      }
-    } else {
-      container.currLine += e.key;
-      term.write(e.key);
-    }
-  });
+    term.onKey((e) => {
+        let code = e.key.charCodeAt();
+
+        if (code == 13) {
+            if (container.currLine) {
+                checkAnswer(container.currLine, qidNum);
+                container.currLine = "";
+                container.cursorPos = 0;
+            }
+        } else if (code < 32) {
+            // Control
+            if (code != 27) {
+                return;
+            }
+
+            switch (e.key) {
+                case '\x1b[D':
+                    if (container.cursorPos > 0) {
+                        container.cursorPos--;
+                        term.write(e.key);
+                    }
+                    break;
+
+                case '\x1b[C':
+                    if (container.cursorPos < container.currLine.length) {
+                        container.cursorPos++;
+                        term.write(e.key)
+                    }
+                    break
+
+                default:
+                    break;
+            }
+        } else if (code == 127 || code == 8) {
+            if (container.currLine) {
+                container.currLine = container.currLine.slice(
+                    0,
+                    container.currLine.length - 1
+                );
+                term.write("\b \b");
+            }
+        } else {
+            container.currLine += e.key;
+            term.write(e.key);
+            container.cursorPos++;
+        }
+
 });
